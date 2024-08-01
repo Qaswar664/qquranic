d997bd031f11f34ff6acfa571549ccccaef78d54
QaswarAhmad <abdullahhasan20202020@gmail.com>
2024-08-02 00:40:59 +0500

    chanages

---
+++ src/routes/ProtectedRoutes.js
@@ -0,0 +1,24 @@
+import React, { Suspense } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import routes from './routes';
+import Loader from 'sharedComponent/Loader';
+
+const ProtectedRoutes = () => (
+  <Switch>
+    <Suspense
+      fallback={<Loader />}
+    >
+      {routes.map(({ component: Component, path, exact }) => (
+        <Route
+          path={`/${path}`}
+          key={path}
+          exact={exact}
+        >
+          <Component />
+        </Route>
+      ))}
+    </Suspense>
+  </Switch>
+);
+
+export default ProtectedRoutes;
