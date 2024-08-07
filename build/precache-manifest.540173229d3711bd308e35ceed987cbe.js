27ec8c3cd0ab0dd557187ab3e3f6709f6f7413f2
QaswarAhmad <qaswarwebtechahmad@gmail.com>
2024-08-07 10:59:45 +0500

    github-pages-deploy

---
+++ build/precache-manifest.540173229d3711bd308e35ceed987cbe.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ab6b547da9e72fa766f",
+    "url": "/qquranic/static/css/main.6fdf2a18.chunk.css"
+  },
+  {
+    "revision": "7ab6b547da9e72fa766f",
+    "url": "/qquranic/static/js/main.07d29065.chunk.js"
+  },
+  {
+    "revision": "b87ec41504403cd942e9",
+    "url": "/qquranic/static/js/runtime~main.8b9d9b6a.js"
+  },
+  {
+    "revision": "e562ba21969900718338",
+    "url": "/qquranic/static/css/2.0144277b.chunk.css"
+  },
+  {
+    "revision": "e562ba21969900718338",
+    "url": "/qquranic/static/js/2.ce5b1304.chunk.js"
+  },
+  {
+    "revision": "20955f4f54aca104b32be8fddc3f709e",
+    "url": "/qquranic/static/media/bg-2.20955f4f.png"
+  },
+  {
+    "revision": "0fbb47b3e003c0ba62cb51eb4118b4f6",
+    "url": "/qquranic/static/media/whyus-rm.0fbb47b3.png"
+  },
+  {
+    "revision": "24e4f40c4afe9ba8ab619b70df46745c",
+    "url": "/qquranic/static/media/headerbg.24e4f40c.webp"
+  },
+  {
+    "revision": "4faf6d087899308c6a098e0fb5e10914",
+    "url": "/qquranic/static/media/menu-logo1.4faf6d08.png"
+  },
+  {
+    "revision": "d053c0aee5599cac05650eaeba8074a1",
+    "url": "/qquranic/static/media/aboutus-img.d053c0ae.png"
+  },
+  {
+    "revision": "27a017a102783e8e0b241f56bd8d64ef",
+    "url": "/qquranic/static/media/mobile-app2.27a017a1.png"
+  },
+  {
+    "revision": "5650e5cf694cfa7e2adf85481188bfd8",
+    "url": "/qquranic/static/media/footer-logo1.5650e5cf.png"
+  },
+  {
+    "revision": "3bf546bf992840a6ae6328a5d5efb0ae",
+    "url": "/qquranic/static/media/tutors.3bf546bf.svg"
+  },
+  {
+    "revision": "b1d13cfa8a0285c7916e7e8f8dacd3ee",
+    "url": "/qquranic/static/media/feeplan.b1d13cfa.webp"
+  },
+  {
+    "revision": "6981233689eb7f180e37b9dd15484af8",
+    "url": "/qquranic/static/media/ceo.69812336.png"
+  },
+  {
+    "revision": "322538e85249511a988b4ac0b7c9ed88",
+    "url": "/qquranic/static/media/courses-image.322538e8.png"
+  },
+  {
+    "revision": "b4b7399bac34d3db1296a5568a574933",
+    "url": "/qquranic/static/media/faqs.b4b7399b.png"
+  },
+  {
+    "revision": "746460655e665fc03b23af8eef643e00",
+    "url": "/qquranic/static/media/signup.74646065.webp"
+  },
+  {
+    "revision": "9a5d18a9fd46ee8fdc8fef20d9e72f51",
+    "url": "/qquranic/static/media/free_trail.9a5d18a9.png"
+  },
+  {
+    "revision": "6962ec0ace5cd60f4ef43954fe624705",
+    "url": "/qquranic/static/media/menu-logo.6962ec0a.png"
+  },
+  {
+    "revision": "b5c46da2d30090f50025aa35bffe55c4",
+    "url": "/qquranic/index.html"
+  }
+];
